4b66f5419ffccff5d7dfed467128de05650a9097
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 14:45:46 +0900

    ✅ subcmd checkout

---
+++ tests/main/test_checkout.py
@@ -0,0 +1,34 @@
+import pytest
+
+from massgit import main
+from tests.utils.init import create_massgit_dir
+from tests.utils.mock import captured_stdouterr
+
+
+@pytest.mark.parametrize(
+    ("mock_def",),
+    [
+        ("checkout/1repo",),
+        ("checkout/2repo",),
+        ("checkout/checkout_file",),
+        ("checkout/some_error",),
+        ("checkout/some_error_reverse",),
+        ("checkout/some_error_checkout_file",),
+    ],
+)
+def test__checkout(
+    mock_subprocess, mock_sep, tmp_cwd, resources, output_detail, mock_def
+):
+    def_mock_subproc = resources.load_mock_subproc(mock_def)
+    output_detail.mock(def_mock_subproc)
+    create_massgit_dir(tmp_cwd, dirnames=def_mock_subproc.repo_dirnames())
+
+    mock_subprocess(def_mock_subproc)
+
+    with captured_stdouterr() as capout:
+        actual_exit_code = main(def_mock_subproc.input_args)
+    out, err = capout.readouterr()
+    output_detail.res(out=out, err=err)
+    assert actual_exit_code == def_mock_subproc.expected_result_code
+    assert out == def_mock_subproc.expected_stdout
+    assert err == def_mock_subproc.expected_stderr
