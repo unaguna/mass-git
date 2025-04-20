7b55a6b8592c84c375afd1a89ac9aee53596a894
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 16:19:15 +0900

    ✅ test unknown_subcmd

---
+++ tests/main/test_unknown_cmd.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+import pytest
+
+from massgit import main
+
+
+def test__unknown_sub_cmd(
+    mock_subprocess,
+    mock_sep,
+    tmp_cwd,
+    tmp_config_dir,
+    resources,
+    output_detail,
+):
+    """変数subcmdsに不備があり正しいサブコマンドの動作定義をロードできない場合。
+
+    通常は発生しない。
+    """
+    import massgit._main.main
+
+    with patch.object(massgit._main.main, "subcmds", dict()):
+        with pytest.raises(Exception):
+            main(["branch"], install_config_dir=tmp_config_dir)
