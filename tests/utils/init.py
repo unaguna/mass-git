1b68e5ea6229d4c73390c396bfabf0f1e428089b
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 16:58:41 +0900

    ✅ env var MASSGIT_REPOS_FILE

--- tests/utils/init.py
+++ tests/utils/init.py
@@ -3,8 +3,14 @@ import os
 import typing as t
 
 
-def create_massgit_dir(cwd: os.PathLike, dirnames: t.Sequence = ("repo1",)):
-    repos_path = os.path.join(cwd, ".massgit", "repos.json")
+def create_massgit_dir(
+    cwd: os.PathLike,
+    dirnames: t.Sequence = ("repo1",),
+    *,
+    repos_path: t.Union[os.PathLike[str], str, None] = None,
+):
+    if repos_path is None:
+        repos_path = os.path.join(cwd, ".massgit", "repos.json")
     os.mkdir(os.path.dirname(repos_path))
 
     repos = [
