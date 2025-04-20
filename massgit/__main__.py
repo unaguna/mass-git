d9d2f78d47f4fb72406170be287281c1019f18b3
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 22:14:05 +0900

    ♻ make __main__ tiny

--- massgit/__main__.py
+++ massgit/__main__.py
@@ -1,11 +1,7 @@
-import os.path
 import sys
 
 from ._main.main import main
 
 if __name__ == "__main__":
-    exit_code = main(
-        sys.argv[1:],
-        install_config_dir=os.path.normpath(os.path.join(__file__, "..", "..", "etc")),
-    )
+    exit_code = main(sys.argv[1:])
     sys.exit(exit_code)
