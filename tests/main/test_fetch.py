47f12754760e713c4408434b61154332deb0c892
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 14:01:13 +0900

    ✅ subcmd fetch

---
+++ tests/main/test_fetch.py
@@ -0,0 +1,30 @@
+import pytest
+
+from massgit import main
+from tests.utils.init import create_massgit_dir
+from tests.utils.mock import captured_stdouterr
+
+
+@pytest.mark.parametrize(
+    ("mock_def",),
+    [
+        ("fetch/1repo",),
+        ("fetch/2repo",),
+        ("fetch/some_error",),
+        ("fetch/some_error_reverse",),
+    ],
+)
+def test__fetch(mock_subprocess, mock_sep, tmp_cwd, resources, output_detail, mock_def):
+    def_mock_subproc = resources.load_mock_subproc(mock_def)
+    output_detail.mock(def_mock_subproc)
+    create_massgit_dir(tmp_cwd, dirnames=def_mock_subproc.repo_dirnames())
+
+    mock_subprocess(def_mock_subproc)
+
+    with captured_stdouterr() as capout:
+        actual_exit_code = main(def_mock_subproc.input_args)
+    out, err = capout.readouterr()
+    output_detail.res(out=out, err=err)
+    assert actual_exit_code == def_mock_subproc.expected_result_code
+    assert out == def_mock_subproc.expected_stdout
+    assert err == def_mock_subproc.expected_stderr
