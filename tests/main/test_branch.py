d517909e8da22aa629bb90b1bcde6c9f0d7eee6f
K-izumi <k.izumi.ysk@gmail.com>
2025-04-20 13:44:51 +0900

    ✅ subcmd branch

---
+++ tests/main/test_branch.py
@@ -0,0 +1,33 @@
+import pytest
+
+from massgit import main
+from tests.utils.init import create_massgit_dir
+from tests.utils.mock import captured_stdouterr
+
+
+@pytest.mark.parametrize(
+    ("mock_def",),
+    [
+        ("branch/1repo",),
+        ("branch/2repo",),
+        ("branch/show-current",),
+        ("branch/some_error",),
+        ("branch/some_error_reverse",),
+    ],
+)
+def test__branch(
+    mock_subprocess, mock_sep, tmp_cwd, resources, output_detail, mock_def
+):
+    def_mock_subproc = resources.load_mock_subproc(mock_def)
+    output_detail.mock(def_mock_subproc)
+    create_massgit_dir(tmp_cwd, dirnames=def_mock_subproc.repo_dirnames())
+
+    mock_subprocess(def_mock_subproc)
+
+    with captured_stdouterr() as capout:
+        actual_exit_code = main(def_mock_subproc.input_args)
+    out, err = capout.readouterr()
+    output_detail.res(out=out, err=err)
+    assert actual_exit_code == def_mock_subproc.expected_result_code
+    assert out == def_mock_subproc.expected_stdout
+    assert err == def_mock_subproc.expected_stderr
