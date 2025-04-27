c376ac2cbea47d529684734af94faa77963f7c5c
K-izumi <k.izumi.ysk@gmail.com>
2025-04-27 17:14:37 +0900

    ✅ error case of subcmd mg-clone

--- tests/main/test_mg_clone.py
+++ tests/main/test_mg_clone.py
@@ -38,3 +38,39 @@ def test__mg_clone(
     assert actual_exit_code == def_mock_subproc.expected_result_code
     assert out == def_mock_subproc.expected_stdout
     assert err == def_mock_subproc.expected_stderr
+
+
+@pytest.mark.parametrize(
+    ("mock_def",),
+    [
+        ("mg_clone/some_no_url",),
+    ],
+)
+def test__mg_clone__error_with_no_url(
+    mock_subprocess,
+    mock_sep,
+    tmp_cwd,
+    tmp_config_dir,
+    resources,
+    output_detail,
+    mock_def,
+):
+    def_mock_subproc = resources.load_mock_subproc(mock_def)
+    output_detail.mock(def_mock_subproc)
+    create_massgit_dir(
+        tmp_cwd,
+        dirnames=["repo1", *def_mock_subproc.repo_dirnames()],
+        no_url_dirnames={"repo1"},
+    )
+
+    mock_subprocess(def_mock_subproc, trap_stderr=True)
+
+    with captured_stdouterr() as capout:
+        actual_exit_code = main(
+            def_mock_subproc.input_args, install_config_dir=tmp_config_dir
+        )
+    out, err = capout.readouterr()
+    output_detail.res(out=out, err=err)
+    assert actual_exit_code == def_mock_subproc.expected_result_code
+    assert out == def_mock_subproc.expected_stdout
+    assert err == def_mock_subproc.expected_stderr
--- tests/utils/init.py
+++ tests/utils/init.py
@@ -3,20 +3,32 @@ import os
 import typing as t
 
 
+class _EmptySet:
+    def __contains__(self, item):
+        return False
+
+
+def _repo(dirname: str, *, no_url_dirnames: t.Set[str]) -> t.Dict[str, t.Any]:
+    repo = {"url": f"http://example.com/{dirname}", "dirname": dirname}
+
+    if dirname in no_url_dirnames:
+        del repo["url"]
+
+    return repo
+
+
 def create_massgit_dir(
     cwd: os.PathLike,
     dirnames: t.Sequence = ("repo1",),
     *,
     repos_path: t.Union[os.PathLike[str], str, None] = None,
+    no_url_dirnames: t.Set[str] = _EmptySet(),
 ):
     if repos_path is None:
         repos_path = os.path.join(cwd, ".massgit", "repos.json")
     os.mkdir(os.path.dirname(repos_path))
 
-    repos = [
-        {"url": f"http://example.com/{dirname}", "dirname": dirname}
-        for dirname in dirnames
-    ]
+    repos = [_repo(dirname, no_url_dirnames=no_url_dirnames) for dirname in dirnames]
 
     with open(repos_path, mode="w") as fp:
         json.dump(repos, fp)
