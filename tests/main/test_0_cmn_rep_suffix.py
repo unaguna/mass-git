8cbcaafe6db6b953a73ed997e5300dc5256a6ea4
K-izumi <k.izumi.ysk@gmail.com>
2025-05-10 19:33:31 +0900

    ✅ option --rep-suffix

--- tests/main/test_0_cmn_rep_suffix.py
+++ tests/main/test_0_cmn_rep_suffix.py
@@ -8,6 +8,51 @@ from tests.utils.mock import captured_stdouterr
 
 
 @pytest.mark.parametrize(
+    ("mock_def", "args_prefix"),
+    [
+        (
+            "0_cmn_rep_suffix/default",
+            ["--rep-suffix", "@@"],
+        ),
+        (
+            "0_cmn_rep_suffix/some_one_line",
+            ["--rep-suffix", "@@ "],
+        ),
+    ],
+)
+def test__change_rep_suffix__by_args(
+    mock_subprocess,
+    mock_sep,
+    tmp_cwd,
+    tmp_config_dir,
+    resources,
+    output_detail,
+    mock_def,
+    args_prefix,
+):
+    def_mock_subproc = resources.load_mock_subproc(mock_def)
+    output_detail.mock(def_mock_subproc)
+    create_massgit_dir(
+        tmp_cwd,
+        dirnames=def_mock_subproc.repo_dirnames(),
+    )
+
+    mocked_subproc = mock_subprocess(def_mock_subproc)
+
+    with captured_stdouterr() as capout:
+        actual_exit_code = main(
+            [*args_prefix, *def_mock_subproc.input_args],
+            install_config_dir=tmp_config_dir,
+        )
+    out, err = capout.readouterr()
+    output_detail.res(out=out, err=err)
+    assert actual_exit_code == def_mock_subproc.expected_result_code
+    assert out == def_mock_subproc.expected_stdout
+    assert err == def_mock_subproc.expected_stderr
+    assert mocked_subproc.assert_call_count()
+
+
+@pytest.mark.parametrize(
     ("mock_def", "env_text"),
     [
         (
