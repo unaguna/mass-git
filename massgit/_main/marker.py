bed570595077d54274a97318260ac534e57d6eda
K-izumi <k.izumi.ysk@gmail.com>
2025-06-11 22:56:36 +0900

    🚧 MarkerProcessor parses boolean expression

--- massgit/_main/marker.py
+++ massgit/_main/marker.py
@@ -1,21 +1,28 @@
+import ast
 import typing as t
+from collections import defaultdict
+from types import CodeType
 
 
 class MarkerProcessor:
-    _markers: t.Sequence[str]
+    _compiled_marker_condition: CodeType
 
-    def __init__(self, marker_condition_args: t.Sequence[str]):
-        self._markers = tuple(
-            part for marker in marker_condition_args for part in marker.split()
-        )
+    def __init__(self, marker_condition: t.Union[str, ast.Expression]):
+        if isinstance(marker_condition, str):
+            marker_condition = ast.parse(marker_condition, mode="eval")
+
+        # TODO: 想定しない構文の場合に例外を発する
+        self._compiled_marker_condition = compile(marker_condition, "<marker>", "eval")
 
     def accept(self, markers: t.Sequence[str]) -> bool:
-        return any(marker in self._markers for marker in markers)
+        globals_d = {
+            # exec, eval 等ビルトイン関数を使用不可にするため、ビルトイン機能を明示する
+            "__builtins__": {},
+        }
+        locals_d = defaultdict(lambda: False, **{m: True for m in markers})
+        return eval(self._compiled_marker_condition, globals_d, locals_d)
 
 
 class AcceptAnyMarkerProcessor(MarkerProcessor):
     def __init__(self):
-        super().__init__([])
-
-    def accept(self, markers: t.Sequence[str]) -> bool:
-        return True
+        super().__init__("True")
--- tests/main/test_marker_processor.py
+++ tests/main/test_marker_processor.py
@@ -4,38 +4,40 @@ from massgit._main.marker import MarkerProcessor, AcceptAnyMarkerProcessor
 
 
 PARAMS_CONTAIN_OR_NOT = (
-    ("marker_condition_args", "markers", "result"),
+    ("marker_condition", "markers", "result"),
     [
-        (["mark1 mark2"], ["mark1"], True),
-        (["mark1 mark2"], ["mark2"], True),
-        (["mark1 mark2"], ["mark1", "mark2"], True),
-        (["mark1 mark2"], ["mark1", "mark0"], True),
-        (["mark1 mark2"], ["mark0", "mark1"], True),
-        (["mark1 mark2"], ["mark0"], False),
-        (["mark1 mark2"], ["mark1 "], False),
-        (["mark1", "mark2"], ["mark1"], True),
-        (["mark1", "mark2"], ["mark2"], True),
-        (["mark1", "mark2"], ["mark1", "mark2"], True),
-        (["mark1", "mark2"], ["mark1", "mark0"], True),
-        (["mark1", "mark2"], ["mark0", "mark1"], True),
-        (["mark1", "mark2"], ["mark0"], False),
-        (["mark1", "mark2"], ["mark1 "], False),
-        (["mark1", "mark2 mark3"], ["mark1"], True),
-        (["mark1", "mark2 mark3"], ["mark2"], True),
-        (["mark1", "mark2 mark3"], ["mark3"], True),
-        (["mark1", "mark2 mark3"], ["mark0"], False),
-        (["mark1", "mark2 mark3"], ["mark1 "], False),
+        ("mark1", ["mark1"], True),
+        ("mark1", ["mark2"], False),
+        ("mark1", ["mark1", "mark2"], True),
+        ("mark1 or mark2", ["mark1"], True),
+        ("mark1 or mark2", ["mark2"], True),
+        ("mark1 or mark2", ["mark1", "mark2"], True),
+        ("mark1 or mark2", ["mark1", "mark0"], True),
+        ("mark1 or mark2", ["mark0", "mark1"], True),
+        ("mark1 or mark2", ["mark0"], False),
+        ("mark1 or mark2", ["mark1 "], False),
+        ("mark1 and mark2", ["mark1"], False),
+        ("mark1 and mark2", ["mark2"], False),
+        ("mark1 and mark2", ["mark1", "mark2"], True),
+        ("mark1 and mark2", ["mark1", "mark0"], False),
+        ("mark1 and mark2", ["mark0", "mark1"], False),
+        ("mark1 and mark2", ["mark0"], False),
+        ("mark1 and mark2", ["mark1 "], False),
+        ("mark1 and (mark2 or mark3)", ["mark1"], False),
+        ("mark1 and (mark2 or mark3)", ["mark1", "mark2"], True),
+        ("mark1 and (mark2 or mark3)", ["mark1", "mark3"], True),
+        ("mark1 and (mark2 or mark3)", ["mark2", "mark3"], False),
     ],
 )
 
 
 @pytest.mark.parametrize(*PARAMS_CONTAIN_OR_NOT)
-def test__marker_processor__contains_or_not(marker_condition_args, markers, result):
-    processor = MarkerProcessor(marker_condition_args)
+def test__marker_processor__contains_or_not(marker_condition, markers, result):
+    processor = MarkerProcessor(marker_condition)
     assert processor.accept(markers) is result
 
 
 @pytest.mark.parametrize(*PARAMS_CONTAIN_OR_NOT)
-def test__marker_processor__contains_all(marker_condition_args, markers, result):
+def test__marker_processor__contains_all(marker_condition, markers, result):
     processor = AcceptAnyMarkerProcessor()
     assert processor.accept(markers)
