906df2162bf28ea3eb479de87dd05fc4377e2e80
K-izumi <k.izumi.ysk@gmail.com>
2025-06-15 13:30:52 +0900

    ✨ allow any() and all() in marker_condition

--- massgit/_main/marker.py
+++ massgit/_main/marker.py
@@ -1,7 +1,6 @@
 import ast
 import copy
 import typing as t
-from collections import defaultdict
 from types import CodeType
 
 
@@ -15,8 +14,8 @@ def marker_expression(value: str) -> ast.Expression:
 
 
 VALID_BUILTINS = {
-    "all": all,
-    "any": any,
+    "all": lambda *args: all(args),
+    "any": lambda *args: any(args),
 }
 
 
@@ -54,7 +53,11 @@ class MarkerProcessor:
             # exec, eval 等ビルトイン関数を使用不可にするため、ビルトイン機能を明示する
             "__builtins__": copy.copy(VALID_BUILTINS),
         }
-        locals_d = defaultdict(lambda: False, **{m: True for m in markers})
+        locals_d = _DefaultDict(
+            {m: True for m in markers},
+            default=False,
+            excluding_keys={*VALID_BUILTINS.keys()},
+        )
         return eval(self._compiled_marker_condition, globals_d, locals_d)
 
     def iter_accepted(
@@ -68,3 +71,52 @@ class MarkerProcessor:
 class AcceptAnyMarkerProcessor(MarkerProcessor):
     def __init__(self):
         super().__init__("True")
+
+
+class _DefaultDict(t.MutableMapping[str, t.Any]):
+    """my defaultdict
+
+    Basically, it behaves the same way as defaultdict.
+    However, the keys in excluding_keys are always kept without values.
+    """
+
+    _default: t.Any
+    _base_dict: t.MutableMapping[str, t.Any]
+    _excluding_keys: t.Set[str]
+
+    def __init__(
+        self,
+        base_dict: t.MutableMapping[str, t.Any],
+        default: t.Any,
+        excluding_keys: t.Set[str],
+    ):
+        self._default = default
+        self._base_dict = base_dict
+        self._excluding_keys = excluding_keys
+
+        for k in self._excluding_keys:
+            if k in self._base_dict:
+                del self._base_dict[k]
+
+    def __getitem__(self, key, /):
+        if key in self._excluding_keys:
+            raise KeyError(key)
+        elif key in self._base_dict:
+            return self._base_dict[key]
+        else:
+            return self._base_dict.setdefault(key, self._default)
+
+    def __len__(self):
+        return len(self._base_dict)
+
+    def __iter__(self):
+        return self._base_dict.__iter__()
+
+    def __setitem__(self, key, value, /):
+        if key in self._excluding_keys:
+            raise KeyError(key)
+        else:
+            self._base_dict.__setitem__(key, value)
+
+    def __delitem__(self, key, /):
+        self._base_dict.__delitem__(key)
