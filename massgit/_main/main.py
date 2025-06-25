ad080f809fdfc0ab75c514bb4712f1fbdc4fe008
K-izumi <k.izumi.ysk@gmail.com>
2025-06-25 10:58:44 +0900

    ✨ allow to output traceback with error message

--- massgit/_main/main.py
+++ massgit/_main/main.py
@@ -4,7 +4,7 @@ import typing as t
 from argparse import ArgumentParser
 
 from ._arg_types import marker_expression
-from ._logging import apply_default_logging_config
+from ._logging import apply_default_logging_config, apply_stderr_logging_config
 from ._params import Params
 from .cmn_each_repo import cmn_each_repo_cmd2
 from .mg_ls_repos import mg_ls_repos_cmd
@@ -46,11 +46,22 @@ subcmd_list = [
 subcmds = {cmd.name(): cmd for cmd in subcmd_list}
 
 
-def _setup_primary_configuration():
+def _setup_primary_configuration(args: argparse.Namespace):
     """Setup primary python configuration"""
 
     # setup logging
-    apply_default_logging_config()
+    if args.log_stderr:
+        if "-full" in args.log_stderr:
+            level = args.log_stderr.removesuffix("-full")
+            is_full = True
+        elif "-" in args.log_stderr:
+            raise ValueError(args.log_stderr)
+        else:
+            level = args.log_stderr
+            is_full = False
+        apply_stderr_logging_config(level, is_full)
+    else:
+        apply_default_logging_config()
 
 
 def _build_main_parser() -> argparse.ArgumentParser:
@@ -66,6 +77,26 @@ def _build_main_parser() -> argparse.ArgumentParser:
         "--rep-suffix",
         required=False,
         help='The suffix of repository name in output. Default is ": " in almost every subcommand.',
+    )
+    group_logging = parser.add_mutually_exclusive_group()
+    group_logging.add_argument(
+        "--log-stderr",
+        default=None,
+        choices=(
+            "ERROR",
+            "WARNING",
+            "INFO",
+            "DEBUG",
+            "ERROR-full",
+            "WARNING-full",
+            "INFO-full",
+            "DEBUG-full",
+        ),
+        help=(
+            "output logs of specified level or more stronger into standard error. "
+            "If '*-full' is specified, "
+            "traceback is also output when an exception occurs, etc."
+        ),
     )
     return parser
 
@@ -99,7 +130,7 @@ def main(
     )
     env = {**os.environ, **dotenv_pub, **dotenv_cwd}
 
-    _setup_primary_configuration()
+    _setup_primary_configuration(main_args)
 
     subcmd = subcmds[main_args.subcmd]
     subcmd_parser = subparsers_dict[subcmd.name()]
